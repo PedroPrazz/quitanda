4a8c41ace5d3bdf5c2a5c3bc1a1512cf0db79a33
PedroPrazz <pedrogprazeres@gmail.com>
2023-09-10 17:32:07 -0300

    criando arquivo custom text field

--- lib/src/sign_in_screen.dart
+++ lib/src/sign_in_screen.dart
@@ -16,11 +16,22 @@ class SignInScreen extends StatelessWidget {
           ),
           Expanded(
             child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 32,
+                vertical: 20,
+              ),
               decoration: const BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.vertical(
                   top: Radius.circular(45),
                 )
+              ),
+              child: Column(
+                children: [
+                  // Email
+                  TextField()
+                  // Senha
+                ],
               ),
             ),
           )
